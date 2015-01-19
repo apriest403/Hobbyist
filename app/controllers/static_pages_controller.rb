135c90e0a5124a39b42be5fd7bc7714f516fafc2
Andrew Priest <apriest403@gmail.com>
2015-01-19 01:40:07 +0000

    Add contact and about

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  
+  def contact
+  end
+  
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#index'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
