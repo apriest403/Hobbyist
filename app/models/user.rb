d42774c6760fcbdf2cbb6201441df04a04a8bad1
Walter <waltertan12@gmail.com>
2015-01-23 21:08:05 +0000

    Finish UserHobby testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ActiveRecord::Base
   has_many :user_hobbies
   has_many :hobbies, :through => :user_hobbies
   
+  def follow(some_hobby)
+    user_hobbies.create(hobby_id: some_hobby.id)
+  end
+  
+  def unfollow(some_hobby)
+    user_hobbies.find_by(hobby_id: some_hobby.id).destroy
+  end
+  
   def has_hobby?(some_hobby)
     hobbies.include?(some_hobby)
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @hobby = Hobby.new(name: "Baseball", description: "Steroids")
-    @user = User.new(name: "Sam", email: "lol@example.com", password: "password", 
+    @hobby = Hobby.create(name: "Baseball", description: "Steroids")
+    @user = User.create(name: "Sam", email: "lol@example.com", password: "password", 
                     password_confirmation: "password")
   end
 
   test "should follow and unfollow a hobby" do
+    assert_not @user.has_hobby?(@hobby)
+    @user.follow(@hobby)
+    assert @user.has_hobby?(@hobby)
+    assert @hobby.users.include?(@user)
+    @user.unfollow(@hobby)
     assert_not @user.has_hobby?(@hobby)
   end
 
