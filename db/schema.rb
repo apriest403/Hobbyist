3aec93ef0b22aad0e6048897cba808139bdff292
Walter <waltertan12@gmail.com>
2015-03-25 19:24:20 -0400

    Fix hobbies controller test by signing in a user before each test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320174001) do
+ActiveRecord::Schema.define(version: 20150125224136) do
 
   create_table "hobbies", force: :cascade do |t|
     t.string   "name"
@@ -32,17 +32,6 @@ ActiveRecord::Schema.define(version: 20150320174001) do
 
   add_index "posts", ["hobby_id"], name: "index_posts_on_hobby_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "follower_id"], name: "index_relationships_on_follower_id_and_follower_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "user_hobbies", force: :cascade do |t|
     t.integer  "user_id"
--- test/controllers/hobbies_controller_test.rb
+++ test/controllers/hobbies_controller_test.rb
@@ -1,22 +1,28 @@
 require 'test_helper'
 
 class HobbiesControllerTest < ActionController::TestCase
-  setup do
-    @hobby = hobbies(:one)
+  def setup
+    @hobby = hobbies(:cooking)
+    @baseball = hobbies(:baseball)
+    @user = User.create(name: "Sam", email: "lol@example.com", password: "password", 
+                    password_confirmation: "password")
   end
 
   test "should get index" do
+    sign_in @user
     get :index
     assert_response :success
     assert_not_nil assigns(:hobbies)
   end
 
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
   test "should create hobby" do
+    sign_in @user
     assert_difference('Hobby.count') do
       post :create, hobby: { name: @hobby.name }
     end
@@ -25,21 +31,25 @@ class HobbiesControllerTest < ActionController::TestCase
   end
 
   test "should show hobby" do
+    sign_in @user
     get :show, id: @hobby
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @hobby
     assert_response :success
   end
 
   test "should update hobby" do
+    sign_in @user
     patch :update, id: @hobby, hobby: { name: @hobby.name }
     assert_redirected_to hobby_path(assigns(:hobby))
   end
 
   test "should destroy hobby" do
+    sign_in @user
     assert_difference('Hobby.count', -1) do
       delete :destroy, id: @hobby
     end
