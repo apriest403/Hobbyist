f460589c77fe6f1ba2c6c1c7a1f878da1a2b4198
Walter <waltertan12@gmail.com>
2015-04-17 15:18:44 -0400

    Update controller tests and fixtures

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,17 @@ class PostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@post = posts(:winning)
+  end
+
+  test "should redirect when not logged in" do
+  	assert_no_difference 'Post.count' do
+  		post :create, post: {title: "nope", content: "hmmm"}
+  	end
+  	assert_redirected_to new_user_session_path
+  end
+
+
 end
--- test/controllers/user_hobbies_controller_test.rb
+++ test/controllers/user_hobbies_controller_test.rb
@@ -4,4 +4,5 @@ class UserHobbiesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
 end
