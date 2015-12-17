a22a0877023ed15e940ad20814090d2080936e01
Walter Tan <waltertan12@gmail.com>
2015-12-16 21:47:18 -0800

    Update user hobby query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     if user_signed_in?
       @user = current_or_guest_user
-      @hobbies = User.includes(:hobbies).find(@user.id).hobbies.paginate(page: params[:page])
+      @hobbies = @user.hobbies.paginate(page: params[:page])
     end
   end
   
