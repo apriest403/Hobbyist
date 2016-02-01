2561508585ce5d63c0c9854abd43ba361d45c5fc
Walter Tan <waltertan12@gmail.com>
2016-02-01 10:55:40 -0800

    Add create and update routes for Posts. Annotate tag and tagging models.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,32 @@
 class Api::PostsController < ApplicationController
+  def create
+    @post = current_user.posts.new(posts_params)
+
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if current_user.id == @post.user_id && @post.update(post_params)
+      render json: @post
+    else
+      render @post.errors.full_messages, status: 422
+    end
+  end
+
   def show
     @post = Post.includes(:user).find(params[:id])
     @all_comments = @post.comments_by_parent_id
     render json: {post: @posts, all_comments: @all_comments}
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :link, :content, :hobby_id)
   end
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: taggings
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer          not null
+#  tag_id     :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Tagging < ActiveRecord::Base
   belongs_to :post
   belongs_to :tag
