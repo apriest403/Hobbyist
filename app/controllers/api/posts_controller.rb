5a763c0cd516c77c8c3b4156ccb19c050b7ff568
Walter Tan <waltertan12@gmail.com>
2016-01-31 21:53:45 -0800

    Don't authenticate user on Api::Posts_controller#show

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,6 @@ class Api::PostsController < ApplicationController
   def show
     @post = Post.includes(:user).find(params[:id])
     @all_comments = @post.comments_by_parent_id
-
     render json: {post: @posts, all_comments: @all_comments}
   end
 end
