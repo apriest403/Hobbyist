3845dfa22d314d95a6c3cd4691378f7061b07c5e
Walter Tan <waltertan12@gmail.com>
2016-02-06 13:34:47 -0700

    Remove precompiled assets

--- app/frontend/entry.js
+++ app/frontend/entry.js
@@ -6,9 +6,6 @@ import App from './containers/app';
 import Dashboard from './components/dashboard';
 import Login from './components/login';
 
-console.log(IndexRoute);
-console.log(Login);
-
 const routes = (
   <Router history={ createHistory() }>
     <Route path='/test' component={ App }>
@@ -16,8 +13,6 @@ const routes = (
     </Route>
   </Router>
 );
-
-console.log(routes);
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(routes, document.getElementById('content'));
